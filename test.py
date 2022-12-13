00cd3a6fd2ddba0dbc6590aaf5e3062193c02cda
SCV22 <108650548+SCV22@users.noreply.github.com>
2022-12-13 23:14:31 +0900

    연습장
    22.12.13

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
-import keyword
-print(keyword.kwlist)
-print("hello world " * 3)
-#파이선은 대소문자를 구분한다
-#키워드는 특별한 의미가 부여된 단어
-#스네이크케이스 예시(item_list)
-#캐멀 케이스 예시(ItemList)
+# #입력
+# string = input("입력> ")
+
+# #출력
+# print("자료:", string)
+# print("자료형:", type(string))
+
+# string_a = input("입력A> ") #문자열 객체 선언
+# int_a = int(string_a) #문자열을 숫자로 바꾸는 int를 이용한 객체 선언
+
+# string_b = input("입력B> ")
+# int_b = int(string_b)
+
+
+# print("문자열 자료:", string_a + string_b)
+# print("숫자 자료:", int_a + int_b)
+
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+print("덧셈결과:", input_a + input_b)
+print("뺄셈결과:", input_a - input_b)
+print("곱셉결과:", input_a * input_b)
+print("나눗셈결과:", input_a / input_b)
